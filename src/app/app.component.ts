55f072e2e210132665b27b89679210b78a81cb84
fjelll <bigoilyman40@gmail.com>
2025-05-26 22:59:52 +0200

    search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./main-page/header/header.component";
-import { SideBarComponent } from "./main-page/side-bar/side-bar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SideBarComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 
