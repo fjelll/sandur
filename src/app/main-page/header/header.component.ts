d91031bd4c89ca491fee37e674d8a3a9a3d82266
fjelll <bigoilyman40@gmail.com>
2025-06-07 23:17:31 +0200

    i fix, im soo good

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [MatSlideToggleModule]
 })
 export class HeaderComponent {}
